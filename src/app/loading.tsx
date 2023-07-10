1b30e1121ff46249105fb86d7a5c0d74ace8e921
Viktor Filippov <jackal3166@gmail.com>
2023-07-10 15:38:38 +0300

    Add Loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,12 +2,11 @@ import Image from 'next/image';
 
 const Loading = () => {
     return (
-        <div className='flex items-center justify-center'>
+        <div className='flex items-center justify-center gap-10 h-[70vh]'>
             <Image
                 src='bean_eater.svg'
                 alt='loading...'
             />
-            <p>Loading...</p>
         </div>
     );
 };
